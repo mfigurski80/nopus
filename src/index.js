37ee70d472031ede449d5cd742aa71911183f641
Mikolaj Figurski <meek.f80@gmail.com>
2021-09-29 17:09:06 -0400

    Add colors from templates

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -17,8 +16,9 @@ ReactDOM.render(
   </Auth0Provider>,
   document.getElementById('root')
 );
-
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import reportWebVitals from './reportWebVitals';
+// reportWebVitals();
