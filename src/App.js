49c98122774323e0668390d23e789737ee152e3c
Mikolaj Figurski <meek.f80@gmail.com>
2021-09-29 16:57:43 -0400

    Add logo, fonts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
@@ -10,6 +9,8 @@ function App() {
   if (isLoading) return <div>Loading ...</div>
   return (
   <>
+    <h1>Login Stuff</h1>
+    <p>This is a small login and style demo</p>
     <LoginButton />
     <LogoutButton />
     <Profile />
