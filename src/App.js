b38d79c4a5d4e413063fd85de857e55417be3e7a
Mikolaj Figurski <meek.f80@gmail.com>
2021-09-29 18:15:11 -0400

    Trade out favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // packages
 import {
-  BrowserRouter as Router, 
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router, 
+    Switch,
+    Route,
+    Link
 } from "react-router-dom"; // https://reactrouter.com/web/guides/quick-start
 // custom components
 import LoginPage from './LoginPage';
@@ -11,17 +11,17 @@ import LoginPage from './LoginPage';
 import './App.css';
 
 function App() {
-  return (
-  <Router>
-    <p>Hi, welcome</p>
-    <Switch>
-      <Route exact path="/">
-        <p>Welcome to the home root "/". Look at the address bar ^^ and add "/login" to go to the login page!</p>
-      </Route>
-      <Route path="/login" component={LoginPage} />
-    </Switch>
-  </Router>
-  );
+    return (
+        <Router>
+            <p>Hi, welcome</p>
+            <Switch>
+                <Route exact path="/">
+                    <p>Welcome to the home root "/". Look at the address bar ^^ and add "/login" to go to the login page!</p>
+                </Route>
+                <Route path="/login" component={LoginPage} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
