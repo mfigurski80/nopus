964ca9fafffbc2f06c963deb118d619b8fc65a30
Mikolaj Figurski <meek.f80@gmail.com>
2021-09-30 18:39:56 -0400

    Add styled components package, base nav styling

--- src/components/ExternalNav.js
+++ src/components/ExternalNav.js
@@ -1,6 +1,7 @@
 import {
     Link
 } from "react-router-dom";
+import styled from 'styled-components';
 
 /**
  * ExternalNav component is used to render 'navbar' for external
@@ -10,10 +11,35 @@ import {
  * @returns {React.Component}
  */
 export default function ExternalNav() {
-    return <div>
-        <h1>External Nav Component</h1>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-    </div>
+    return <Nav>
+        <NavLinkImage to="/">
+            <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="logo" width="51px" height="40px"/>
+        </NavLinkImage>
+        <NavLink to="/features">Features</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <Spacer />
+        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/login">Login</NavLink>
+    </Nav>
 }
+
+const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 15px 80px;
+`
+const Spacer = styled.div`
+    flex: 1;
+`
+const NavLink = styled(Link)`
+    padding: 30px 20px;
+    text-decoration: none;
+    color: inherit;
+    font-weight: bold;
+`
+
+const NavLinkImage = styled(NavLink)`
+    padding-top: 0;
+    padding-bottom: 0;
+`
