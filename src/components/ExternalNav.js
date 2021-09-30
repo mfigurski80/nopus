663bd77ad1d8e82b1b3549f4e7b1cc0b2abecf33
Mikolaj Figurski <meek.f80@gmail.com>
2021-09-30 18:54:51 -0400

    Add navigation buttons styling

--- src/components/ExternalNav.js
+++ src/components/ExternalNav.js
@@ -1,7 +1,7 @@
 import {
     Link
 } from "react-router-dom";
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 /**
  * ExternalNav component is used to render 'navbar' for external
@@ -19,24 +19,35 @@ export default function ExternalNav() {
         <NavLink to="/about">About</NavLink>
         <Spacer />
         <NavLink to="/signup">Sign Up</NavLink>
-        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/login" solid>Login</NavLink>
     </Nav>
 }
 
 const Nav = styled.nav`
     display: flex;
     align-items: center;
-    gap: 10px;
-    padding: 15px 80px;
+    gap: 45px;
+    max-width: 1400px;
+    margin: 0 auto;
+    padding: 25px 20px;
 `
 const Spacer = styled.div`
     flex: 1;
 `
 const NavLink = styled(Link)`
-    padding: 30px 20px;
+    padding: 10px 5px;
     text-decoration: none;
     color: inherit;
     font-weight: bold;
+    ${props => props.solid && css`
+        background: var(--color-primary);
+        color: #fff;
+        border-radius: 10px;
+        padding: 12px 23px;
+        &:hover {
+            background: var(--color-primary-dark);
+        }
+    `}
 `
 
 const NavLinkImage = styled(NavLink)`
