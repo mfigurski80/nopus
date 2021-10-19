9d61a0605dba7cd7c75bfe1429576a061e83bfc6
Mikolaj Figurski <meek.f80@gmail.com>
2021-10-19 14:59:01 -0400

    Demonstrate class on schedule

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -2,34 +2,42 @@ import styled from 'styled-components';
 
 import { listHours } from 'utils';
 
-export default function Schedule({schedule, timeRange: [ start, end ]}) {
+export default function Schedule({timeRange: [ start, end ]}) {
+
+    let schedule = {
+        monday: [
+            {start: 60*9, end: 60*10 + 30, title: 'Приветствие'},
+        ],
+    };
+    
     let nRows = (end-start) / 15;
     return (
-        <>
-            <WeekRow>
-                <TimeColumn nRows={nRows/4}>
-                    <p></p>
-                    { listHours(start, end).map(h => (
-                        <h4 id={h}>{h}</h4>
-                    )) }
-                </TimeColumn>
-                <DayColumn nRows={nRows}>
-                    <DayHeader>Monday</DayHeader>
-                </DayColumn>
-                <DayColumn nRows={nRows}>
-                    <DayHeader>Tuesday</DayHeader>
-                </DayColumn>
-                <DayColumn nRows={nRows}>
-                    <DayHeader>Wednesday</DayHeader>
-                </DayColumn>
-                <DayColumn nRows={nRows}>
-                    <DayHeader>Thursday</DayHeader>
-                </DayColumn>
-                <DayColumn nRows={nRows}>
-                    <DayHeader>Friday</DayHeader>
-                </DayColumn>
-            </WeekRow>
-        </>
+        <WeekRow>
+            <TimeColumn nRows={nRows/4}>
+                <p></p>
+                { listHours(start, end).map(h => (
+                    <h4 id={h}>{h}</h4>
+                )) }
+            </TimeColumn>
+            <DayColumn nRows={nRows}>
+                <DayHeader>Monday</DayHeader>
+                { schedule.monday?.map(({start:s, end:e, title}) => (
+                    <EventItem start={(s-start) / 15} end={(e-end) / 15}>{title}</EventItem>
+                ))}
+            </DayColumn>
+            <DayColumn nRows={nRows}>
+                <DayHeader>Tuesday</DayHeader>
+            </DayColumn>
+            <DayColumn nRows={nRows}>
+                <DayHeader>Wednesday</DayHeader>
+            </DayColumn>
+            <DayColumn nRows={nRows}>
+                <DayHeader>Thursday</DayHeader>
+            </DayColumn>
+            <DayColumn nRows={nRows}>
+                <DayHeader>Friday</DayHeader>
+            </DayColumn>
+        </WeekRow>
     );
 }
 
@@ -38,7 +46,6 @@ const WeekRow = styled.div`
     grid-template-columns: 80px repeat(5, 1fr);
     grid-gap: 4px;
     width: 100%;
-    background: pink;
     & > * {
         width: 100%;
     }
@@ -51,13 +58,21 @@ const DayHeader = styled.h3`
     text-align: center;
 `;
 
+const HEADER_HEIGHT = 40;
+const QUARTER_HOUR_HEIGHT = 20;
+
 const TimeColumn = styled.div`
     display: grid;
-    grid-template-rows: 40px repeat(${({nRows}) => nRows}, 60px);
-    align-items: center;
+    grid-template-rows: ${HEADER_HEIGHT}px repeat(${({nRows}) => nRows}, ${QUARTER_HOUR_HEIGHT * 4}px);
+    // align-items: center;
 `;
 
 const DayColumn = styled.div`
     display: grid;
-    grid-template-rows: 40px repeat(${({nRows}) => nRows}, 15px);
+    grid-template-rows: ${HEADER_HEIGHT}px repeat(${({nRows}) => nRows}, ${QUARTER_HOUR_HEIGHT}px);
+`;
+
+const EventItem = styled.div`
+    background: #eee;
+    grid-row: ${({start}) => start + 2} / ${({end}) => end - 1};
 `;
--- src/utils/listHours.js
+++ src/utils/listHours.js
@@ -1,6 +1,6 @@
 export function listHours(from, to) {
     const hours = [];
-    for (let i = from/60; i <= to/60; i++) {
+    for (let i = from/60; i < to/60; i++) {
         let hr = i%12 === 0 ? 12 : i%12;
         hours.push(`${hr}${i>12 ? 'pm' : 'am'}`);
     }
