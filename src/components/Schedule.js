241432ce8e430d15a8fbe1970b995881b7c6c2a7
Mikolaj Figurski <meek.f80@gmail.com>
2021-10-25 13:05:55 -0400

    Isolate data from schedule component

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -4,13 +4,8 @@ import { listHours, scheduleByDay, stringToColor } from 'utils';
 
 const WEEK_NAMES = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-export default function Schedule({timeRange: [ start, end ]}) {
+export default function Schedule({schedule, timeRange: [ start, end ]}) {
 
-    const schedule = [
-        { start: 60*9, end: 60*10 + 30, name: 'CS 170', days: [0,2] },
-        { start: 60*10, end: 60*11, name: 'CS 171', days: [1,3] },
-        { start: 60*14, end: 60*15, name: 'CS 220', days: [0,2,4] },
-    ];
     const dailySchedule = scheduleByDay(schedule);
 
     let nRows = (end-start) / 15;
--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -3,10 +3,17 @@ import { Container } from '@material-ui/core';
 import Schedule from 'components/Schedule';
 
 export default function SchedulePage() {
+    
+    const schedule = [
+        { start: 60*9, end: 60*10 + 30, name: 'CS 170', days: [0,2] },
+        { start: 60*10, end: 60*11, name: 'CS 171', days: [1,3] },
+        { start: 60*14, end: 60*15, name: 'CS 220', days: [0,2,4] },
+    ];
+
     return (
         <Container>
             <h1>Schedule</h1>
-            <Schedule timeRange={[8*60, 18*60]}/>
+            <Schedule schedule={schedule} timeRange={[8*60, 18*60]}/>
         </Container>
     );
 }
