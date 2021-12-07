82707e20773fbd83b9237b67558d70995bc0aa59
Eric Tiancheng Gu <gudmaneric@gmail.com>
2021-12-06 22:56:58 -0500

    hardcode

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -50,9 +50,9 @@ function allyProps(index) {
 export default function SchedulePage() {
     const { user } = useAuth0();
     const [ schedule, setSchedule ] = React.useState([
-      { start: 60*9, end: 60*10 + 30, name: 'CS 170', days: [0,2] },
-      { start: 60*10, end: 60*11, name: 'CS 171', days: [1,3] },
-      { start: 60*14, end: 60*15, name: 'CS 224', days: [0,2,4] },
+      { start: 60*9, end: 60*10 + 30, name: 'CS 171', days: [0,2] },
+      { start: 60*10, end: 60*11, name: 'MATH 111', days: [1,3] },
+      { start: 60*14, end: 60*15, name: 'PHIL 110', days: [0,2,4] },
   ]);
     
     useEffect(async () => {
